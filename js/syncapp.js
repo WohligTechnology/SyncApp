c5b6f1e0e4c2f2f37412396ebcc9e6b300bf1345
Chintan Shah <chintan@wohlig.com>
2015-05-19 11:00:58 +0530

    app sync changed

--- js/syncapp.js
+++ js/syncapp.js
@@ -1,6 +1,7 @@
 var adminurl = "http://localhost/syncbackend/index.php/welcome/";
 var config = {};
-
+var abc = 0;
+var user = 3;
 var syncapp = angular.module('syncapp', []);
 
 syncapp.factory('SyncApp', function ($http) {
@@ -19,26 +20,35 @@ syncapp.factory('SyncApp', function ($http) {
     };
 
     function changeservertimestamp(timestamp) {
-        config.user.servertimestamp = data.timestamp;
+        config.user.servertimestamp = timestamp;
         setconfig();
     };
+
     var db = openDatabase('sync', '1.0', 'SyncTestDatabase', 2 * 1024 * 1024);
-    
+
     db.transaction(function (tx) {
-        tx.executeSql('CREATE TABLE IF NOT EXISTS `users` (`id` INTEGER PRIMARY KEY ASC, `name` VARCHAR(255),`email` VARCHAR(255),`serverid` INTEGER UNIQUE )');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS `users` (`id` INTEGER PRIMARY KEY ASC, `name` VARCHAR(255),`email` VARCHAR(255),`serverid` INTEGER  )');
     });
     db.transaction(function (tx) {
-        tx.executeSql('CREATE TABLE IF NOT EXISTS `userslog` (id INTEGER PRIMARY KEY ASC, `timestamp` TIMESTAMP,`type` INTEGER,`user` INTEGER)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS `userslog` (`id` INTEGER PRIMARY KEY ASC, `timestamp` TIMESTAMP,`type` INTEGER,`user` INTEGER,`table` INTEGER)');
     });
 
     returnval.query = function (querystr, callback) {
         db.transaction(function (tx) {
             tx.executeSql(querystr, [], function (tx, results) {
+
                 var len = results.rows.length;
+                console.log(results);
+                abc = results;
                 if (callback) {
-                    callback(results.rows, len);
+
+                    callback(results.rows, len, results);
+
                 }
-            }, null);
+            }, function (tx, error) {
+                abc = error;
+                console.log(error);
+            });
         });
     };
 
@@ -50,7 +60,7 @@ syncapp.factory('SyncApp', function ($http) {
         case "1":
             {
                 returnval.query("INSERT INTO `users` (`id`, `name`,`email`,`serverid`) VALUES (null,'" + data.name + "','" + data.email + "','" + data.id + "')", function (result, len) {
-                    
+
                     callback();
                 });
             }
@@ -77,14 +87,36 @@ syncapp.factory('SyncApp', function ($http) {
     }
 
 
-    returnval.create = function (user, callback) {
+    returnval.create = function (data, callback) {
+
+        returnval.query("INSERT INTO `users` (`id`, `name`,`email`,`serverid`) VALUES (null,'" + data.name + "','" + data.email + "','" + data.id + "')", function (result, len, id) {
+            id=id.insertId;
+            var d = new Date();
+            var n = d.getTime();
+            returnval.query("INSERT INTO `userslog` (`id`,`timestamp`,`type`,`user`,`table`) VALUES (null,'" + n + "','" + 1 + "','" + user + "','" + id + "')", null);
+            console.log(id);
+
+            callback();
+        });
+    };
+    returnval.update = function (data, callback) {
+
+        returnval.query("UPDATE `users` SET `name`='" + data.name + "',`email`='" + data.email + "' WHERE `id`='" + data.id + "'", function (result, len) {
+
+            var d = new Date();
+            var n = d.getTime();
+            returnval.query("INSERT INTO `userslog` (`id`,`timestamp`,`type`,`user`,`table`) VALUES (null,'" + n + "','" + 2 + "','" + user + "','" + data.id + "')", null);
+            callback();
+        });
 
     };
-    returnval.update = function (user, callback) {
-
-    };
-    returnval.delete = function (id, callback) {
-
+    returnval.delete = function (data, callback) {
+        returnval.query("DELETE FROM `users` WHERE `id`='" + data.id + "'", function (result, len) {
+            var d = new Date();
+            var n = d.getTime();
+            returnval.query("INSERT INTO `userslog` (`id`,`timestamp`,`type`,`user`,`table`) VALUES (null,'" + n + "','" + 3 + "','" + user + "','" + data.id + "')", null);
+            callback();
+        });
     };
 
     returnval.servertolocal = function () {
